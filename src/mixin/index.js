d2a6e2666c0c8c45abfb16c694b59331b2c881af
Florian Klampfer <mail@qwtel.com>
2018-04-10 14:10:04 +0200

    lazy loading cancellation WIP

--- src/mixin/index.js
+++ src/mixin/index.js
@@ -16,12 +16,51 @@
 
 // Importing the hy-compontent base libary,
 // which helps with making multiple versions of the component (Vanilla JS, WebComponent, etc...).
-import {
-  componentMixin,
-  COMPONENT_FEATURE_TESTS,
-  Set
-} from "hy-component/src/component";
+import { componentMixin, COMPONENT_FEATURE_TESTS, Set } from "hy-component/src/component";
 import { arrayOf, number, string } from "hy-component/src/types";
+
+import { Observable } from "rxjs/_esm5/Observable";
+import { Subject } from "rxjs/_esm5/Subject";
+
+import { never } from "rxjs/_esm5/observable/never";
+
+import { ajax } from "rxjs/_esm5/observable/dom/ajax";
+
+/* import { distinctUntilKeyChanged } from "rxjs/_esm5/operators/distinctUntilKeyChanged"; */
+import { filter } from "rxjs/_esm5/operators/filter";
+import { map } from "rxjs/_esm5/operators/map";
+/* import { partition } from "rxjs/_esm5/operators/partition"; */
+import { switchMap } from "rxjs/_esm5/operators/switchMap";
+import { take } from "rxjs/_esm5/operators/take";
+import { takeUntil } from "rxjs/_esm5/operators/takeUntil";
+import { tap } from "rxjs/_esm5/operators/tap";
+
+/*
+function blobToDataURL(blob) {
+  return new Promise((res, rej) => {
+    const a = new FileReader();
+    a.onload = ({ target: { result } }) => res(result);
+    a.onerror = rej;
+    a.readAsDataURL(blob);
+  });
+}
+*/
+
+const createXObservable = X => (el, opts) =>
+  Observable.create(obs => {
+    const next = obs.next.bind(obs);
+    const observer = new X(xs => Array.from(xs).forEach(next), opts);
+    observer.observe(el);
+    return () => observer.unobserve(el);
+  });
+
+const createIntersectionObservable = createXObservable(IntersectionObserver);
+const createResizeObservable = createXObservable(ResizeObserver);
+
+// Consider a URL external if either the protocol, hostname or port is different.
+function isExternal({ protocol, host }, location = window.location) {
+  return protocol !== location.protocol || host !== location.host;
+}
 
 // A set of [Modernizr] tests that are required for this component to work.
 export const MIXIN_FEATURE_TESTS = new Set([
@@ -77,10 +116,74 @@ export const imageMixin = C =>
     // Overriding the setup function.
     setupComponent(el, props) {
       super.setupComponent(el, props);
+      this.teardown$ = new Subject();
+    }
+
+    createImage(url) {
+      /* if (process.env.DEBUG) console.log("create image", this.el.getAttribute("src")); */
+      this.img = document.createElement("img");
+      /* this.img.style.width = "100%"; */
+      this.img.src = url;
+      if (this.el.hasAttribute("alt")) this.img.setAttribute("alt", this.el.getAttribute("alt"));
+      this.el.appendChild(this.img);
     }
 
     // Calling the [setup observables function](./setup.md) function.
     connectComponent() {
+      /*
+      this.el.style.display = "inline-block";
+      this.el.style.position = "relative";
+      */
+
+      if ("IntersectionObserver" in window) {
+        createIntersectionObservable(this.el)
+          .pipe(
+            takeUntil(this.teardown$),
+            /* distinctUntilKeyChanged("isIntersecting"), */
+            switchMap(({ isIntersecting }) => {
+              // TODO: use js implementation of srcset (+sizes) to determine size
+              // TODO: use resize observer to keep src up-2-date
+              const url = new URL(this.el.getAttribute("src"), window.location);
+
+              return !isIntersecting
+                ? never()
+                : ajax({
+                    method: "GET",
+                    url,
+                    crossDomain: isExternal(url),
+                    responseType: "blob"
+                  });
+            }),
+            take(1),
+            map(({ response }) => URL.createObjectURL(response))
+            /* switchMap(({ response }) => blobToDataURL(response)) */
+          )
+          .subscribe(
+            url => {
+              this.createImage(url);
+              this.objectURL = url;
+            },
+            err => {
+              if (process.env.DEBUG) console.error(err);
+              this.createImage(this.el.getAttribute("src"));
+            }
+          );
+
+        /*
+        this.intersectionObserver = new IntersectionObserver(this.intersectionCallback.bind(this), {
+          root: this.root,
+          rootMargin: (this.padding || [0]).map(x => `${x}px`).join(" ")
+        });
+        this.intersectionObserver.observe(this.el);
+        */
+      } else {
+        // When no intersection observer, just load image
+        this.loadImage();
+      }
+
+      // Firing an event to let the outside world know the drawer is ready.
+      this.fireEvent("init");
+
       /*
       const scrollEl = this.root == null ? window : document.querySelector(this.root);
       const { observers } = this.constructor;
@@ -96,35 +199,58 @@ export const imageMixin = C =>
       console.log(observers);
       this.observer.observe(this.el);
       */
-
-      if ("IntersectionObserver" in window) {
-        this.intersectionObserver = new IntersectionObserver(
-          this.intersectionCallback.bind(this),
-          {
-            root: this.root,
-            rootMargin: this.padding.map(x => `${x}px`).join(" ")
-          }
-        );
-        this.intersectionObserver.observe(this.el);
-      } else {
-        // When no intersection observer, just load image
-        this.loadImage();
-      }
-
-      // Firing an event to let the outside world know the drawer is ready.
-      this.fireEvent("init");
     }
 
+    /*
     intersectionCallback(entries) {
-      // console.log(entries.length);
       entries.forEach(entry => {
-        if (entry.isIntersecting && !this.done) {
+        if (entry.isIntersecting && !this.done && !this.inProgress) {
           this.loadImage();
+        }
+        if (!entry.isIntersecting && this.inProgress) {
+          this.cancelLoadImage();
         }
       });
     }
+    */
 
     loadImage() {
+      /*
+      ajax({
+        method: "GET",
+        responseType: "blob",
+        url: this.getAttribute("src")
+        // crossDomain: isExternal(this)
+      })
+        .subscribe(({ response }) => console.log(response));
+      */
+      /*
+      const noscript = this.el.querySelector("noscript");
+      const div = document.createElement("div");
+      div.innerHTML = noscript.textContent;
+      const [img] = div.childNodes;
+      // img.src = "";
+      this.img = img;
+
+      console.log("load", img);
+
+      if (process.env.DEBUG && img.tagName !== "IMG") {
+        console.log("Content of <noscript> does not appear to be an <img>", img);
+      }
+
+      this.inProgress = true;
+      img.addEventListener("load", () => {
+        console.log("loaded", img);
+        this.el.appendChild(img);
+        this.inProgress = false;
+        this.done = true;
+      });
+
+      window.requestAnimationFrame(() => {
+        this.fireEvent("load-img", { detail: img });
+        this.el.removeChild(noscript);
+      });
+      */
       /*
       const img = (this.done = new Image());
       img.classList = this.el.classList;
@@ -141,28 +267,21 @@ export const imageMixin = C =>
       // img.onerror =...
       this.el.appendChild(img);
       */
-
-      const noscript = this.el.querySelector("noscript");
-      const temp = document.createElement("div");
-      temp.innerHTML = noscript.textContent;
-      const [img] = temp.childNodes;
-      if (process.env.DEBUG && img.tagName !== "IMG") {
-        console.log(
-          "Content of <noscript> does not appear to be an <img>",
-          img
-        );
-      }
-      this.fireEvent("load-img", { detail: img });
-
-      this.el.removeChild(noscript);
-      this.el.appendChild(img);
-
-      this.done = true;
     }
 
+    /*
+    cancelLoadImage() {
+      const { img } = this;
+      console.log("cancel", img);
+      img.src = "";
+      this.inProgress = false;
+      // window.requestAnimationFrame(() => this.el.removeChild(img));
+    }
+    */
+
     disconnectComponent() {
-      /* this.teardown$.next({}); */
-      this.intersectionObserver.unobserve(this.el);
+      if (this.objectURL) URL.revokeObjectURL(this.objectURL);
+      this.teardown$.next({});
     }
 
     adoptComponent() {
--- src/webcomponent/index.js
+++ src/webcomponent/index.js
@@ -32,9 +32,7 @@ export const WEBCOMPONENT_FEATURE_TESTS = new Set([
 
 export { Set };
 
-export class ShyImageElement extends customElementMixin(
-  imageMixin(CustomElement)
-) {
+export class ShyImageElement extends customElementMixin(imageMixin(CustomElement)) {
   static get observedAttributes() {
     return this.getObservedAttributes();
   }
