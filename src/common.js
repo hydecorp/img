32ef472ca917e400376db7edae9751e08e16a004
Florian Klampfer <mail@qwtel.com>
2018-06-29 20:15:29 +0700

    update to rxjs 6

--- src/common.js
+++ src/common.js
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import { Observable } from "rxjs/_esm5/Observable";
+import { Observable } from "rxjs/_esm5";
 
 export const hasCSSOM = "attributeStyleMap" in Element.prototype && "CSS" in window && CSS.number;
 
