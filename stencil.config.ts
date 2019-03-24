cc540093f7ee584539be0ed024e8e4f617b48e43
Florian Klampfer <mail@qwtel.com>
2019-03-24 15:26:10 +0700

    Update

--- stencil.config.ts
+++ stencil.config.ts
@@ -2,8 +2,10 @@ import { Config } from '@stencil/core';
 
 export const config: Config = {
   namespace: 'hy-img',
+  minifyJs: false,
   outputTargets:[
     { type: 'dist' },
+    { type: 'www' },
     { type: 'docs' },
     // { type: 'www' },
   ],
